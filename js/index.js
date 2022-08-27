feb862cf612a45566fe48f0b9cfc91f4db54f5c4
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2022-08-27 14:58:00 -0700

    Created level layout and changed array from blocks to level

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 var keyUp;
 var keyDown;
 var keyLeft;
 var keyRight;
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-console.log(canvas.width);
-
+//Parent class for Player subclass
 class Block {
     constructor(x, y, width, height, color) {
         this.x = x;
@@ -19,8 +18,16 @@ class Block {
         this.height = height;
         this.color = color;
     }
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fill();
+    }
 }
 
+//Extension of Block with added velocity
 class Player extends Block {
     constructor(x, y, width, height, color) {
         super(x, y, width, height, color);
@@ -29,16 +36,8 @@ class Player extends Block {
         this.maxspeed = 2;
         this.active = true;
     }
-    
-    
 
-    draw() {
-        ctx.beginPath();
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x , this.y, this.width, this.height);
-        ctx.fill();
-    }
-
+    //For player movement on both WASD and Arrows
     step() {
         if (this.active) {
             if (!keyLeft && !keyRight || keyLeft && keyRight) {
@@ -75,10 +74,8 @@ class Player extends Block {
                 }
             }
 
-            if (detectCollision(player, blocks)) {
+            if (detectCollision(player, level)) {
                 this.maxspeed = 0;
-            } else {
-                console.log('eeee')
             }
         }
 
@@ -89,35 +86,22 @@ class Player extends Block {
 
 }
 
-class Maze extends Block {
-    constructor(x, y, width, height, color) {
-        super(x, y, width, height, color);
-    }
-
-    draw() {
-        ctx.beginPath();
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.fill();
-    }
-}
-
 function animate() {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < blocks.length; i++) {
-        blocks[i].draw();
+    for (let i = 0; i < level.length; i++) {
+        level[i].draw();
     }
     player.step();
     
 }
 
-function detectCollision(player, blocks) {
-    for (let i = 0; i < blocks.length; i++) {
-        if (player.x <= blocks[i].x + blocks[i].width &&
-            player.x + player.width >= blocks[i].x &&
-            player.y <= blocks[i].y + blocks[i].height &&
-            player.y + player.height >= blocks[i].y) {
+function detectCollision(player, level) {
+    for (let i = 0; i < level.length; i++) {
+        if (player.x <= level[i].x + level[i].width &&
+            player.x + player.width >= level[i].x &&
+            player.y <= level[i].y + level[i].height &&
+            player.y + player.height >= level[i].y) {
                 return true;
             }
     }
@@ -149,13 +133,22 @@ function setupInputs() {
     })
 }
 
-const player = new Player(canvas.width / 2, canvas.height / 2, 15, 15, 'red');
+const player = new Player(canvas.width / 2 - 475, canvas.height / 2 + 255, 10, 10, 'red');
 
-var blocks = [
-    new Maze(canvas.width / 2 - 500, canvas.height / 2 + 50, 1000, 500, 'black'),
-    new Maze(canvas.width / 2 - 500, canvas.height / 2 + 50, 1000, 500, 'blue'),
-    new Maze(canvas.width / 2 + 500, canvas.height / 2 - 500, 500, 2000, 'red'),
-    new Maze(canvas.width / 2 - 500, canvas.height / 2 + 300, 1000, 200, 'yellow'),
+var level = [
+    new Block(canvas.width / 2 - 1000, canvas.height / 2 - 500, 500, 1000, 'black'),
+    new Block(canvas.width / 2 - 500, canvas.height / 2 - 500, 1000, 200, 'black'),
+    new Block(canvas.width / 2 + 500, canvas.height / 2 - 500, 500, 1000, 'black'),
+    new Block(canvas.width / 2 - 500, canvas.height / 2 + 300, 1000, 200, 'black'),
+    new Block(canvas.width / 2 - 500, canvas.height / 2 + 175, 950, 50, 'black'),
+    new Block(canvas.width / 2 - 450, canvas.height / 2 - 75, 1000, 200, 'black'),
+    new Block(canvas.width / 2 + 50, canvas.height / 2 - 310, 1000, 250, 'black'),
+    new Block(canvas.width / 2 - 500, canvas.height / 2 - 315, 500, 205, 'black'),
+    new Block(canvas.width / 2 - 465, canvas.height / 2 - 185, 500, 75, 'black'),
+    new Block(canvas.width / 2 + 15, canvas.height / 2 - 230, 50, 30, 'black'),
+    new Block(canvas.width / 2 - 25, canvas.height / 2 - 260, 50, 15, 'black'),
+    new Block(canvas.width / 2 + 40, canvas.height / 2 - 260, 50, 35, 'black'),
+    new Block(canvas.width / 2 + 30, canvas.height / 2 - 95, 50, 35, 'black')
     
 ];
 
